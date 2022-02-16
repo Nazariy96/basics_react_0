1d23de7b4f04eaf55c13c5562b13f1e3c58aae6d
NazariyAT <andriyishyn.tenetyuk@bootcamp-upgrade.com>
2022-02-16 09:02:10 +0100

    alt carousel

--- src/__BASICS__/Carousel/Carousel.jsx
+++ src/__BASICS__/Carousel/Carousel.jsx
@@ -1,30 +1,32 @@
 import { useState } from "react";
 
-export default function Carousel(props) {
+export default function Carousel({images}) {
+
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [selectedImage, setSelectedImage] = useState(props.images[0]);
 
-
-  const selectNewImage = (index, images, next) => { 
+  /* const selectNewImage = (index, images, next) => { 
     const condition = next ? index < images.length - 1 : index > 0;
     const nextIndex = next ? (condition ? index + 1 : 0) : condition ? index - 1 : images.length - 1;
-    setSelectedImage(images[nextIndex]);
     setSelectedIndex(nextIndex);
-  };
+  }; */
 
   const previous = () => {
-    selectNewImage(selectedIndex, props.images, false);
+    /* selectNewImage(selectedIndex, images, false); */
+    const nextIndex = selectedIndex > 0 ? selectedIndex - 1 : images.length - 1
+    setSelectedIndex(nextIndex)
   };
 
   const next = () => {
-    selectNewImage(selectedIndex, props.images, true);
+    /* selectNewImage(selectedIndex, images, true); */
+    const nextIndex = selectedIndex < images.length - 1 ? selectedIndex + 1 : 0
+    setSelectedIndex(nextIndex)
   };
 
   return (
     <>
       <div>
-        <p>{selectedImage.id}</p>
-        <img src={selectedImage.url} alt={selectedImage.title} width="100px" height="100px"/>
+        <p>{images[selectedIndex].id}</p>
+        <img src={images[selectedIndex].url} alt={images[selectedIndex].title} width="100px" height="100px"/>
         
         <button onClick={previous}>{"<"}</button>
         <button onClick={next}>{">"}</button>
